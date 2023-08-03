831c0f6f0ae63be58a50a101a3d81497cb618a01
Kai-Chu Chung <cage.chung@gmail.com>
2023-08-03 20:20:21 +0800

    refactor: remove update functionality from root command
    - Remove the &#34;CheckForUpdate&#34; function in `cmd/root.go`
    - Remove the import of &#34;os/exec&#34; in `cmd/root.go`
    - Delete the entire `cmd/update.go` file, which includes the removal of the &#34;updateCmd&#34; command
    
    Signed-off-by: Kai-Chu Chung <cage.chung@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,51 +1,28 @@
 /*
 Copyright © 2023 KAI CHU CHUNG
-
 */
 package cmd
 
 import (
 	"log"
 	"os"
-	"os/exec"
 
 	aw "github.com/deanishe/awgo"
 	"github.com/deanishe/awgo/update"
 	"github.com/spf13/cobra"
 )
 
-const updateJobName = "checkForUpdate"
-
 var (
 	repo = "cage1016/alfred-xlsx2csv"
 	wf   *aw.Workflow
 )
-
-func CheckForUpdate() {
-	if wf.UpdateCheckDue() && !wf.IsRunning(updateJobName) {
-		log.Println("Running update check in background...")
-		cmd := exec.Command(os.Args[0], "update")
-		if err := wf.RunInBackground(updateJobName, cmd); err != nil {
-			log.Printf("Error starting update check: %s", err)
-		}
-	}
-
-	if wf.UpdateAvailable() {
-		wf.Configure(aw.SuppressUIDs(true))
-		wf.NewItem("An update is available!").
-			Subtitle("⇥ or ↩ to install update").
-			Valid(false).
-			Autocomplete("workflow:update").
-			Icon(&aw.Icon{Value: "update-available.png"})
-	}
-}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "Excel to CSV",
 	Short: "Convert Excel to CSV",
 	Run: func(cmd *cobra.Command, args []string) {
-		CheckForUpdate()
+
 		wf.SendFeedback()
 	},
 }
